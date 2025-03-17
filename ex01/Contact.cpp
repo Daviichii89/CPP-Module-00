484b42a0b7b91d911253ecaf74bcdc29c9060209
David Fernandez <davichi89@gmail.com>
2025-03-17 21:44:08 +0100

    Create class Contact in the header file and create the functions

---
+++ ex01/Contact.cpp
@@ -0,0 +1,19 @@
+#include "Contact.hpp"
+
+Contact::Contact() {}
+
+void Contact::setContact(std::string fName, std::string lName, std::string phone) {
+    firstName = fName;
+    lastName = lName;
+    phoneNumber = phone;
+}
+
+void Contact::displayContact() const {
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Phone Number: " << phoneNumber << std::endl;
+}
+
+std::string Contact::getSummary() const {
+    return firstName + " | " + lastName;
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,20 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string phoneNumber;
+
+public:
+    Contact();
+    void setContact(std::string fName, std::string lName, std::string phone);
+    void displayContact() const;
+    std::string getSummary() const;
+};
+
+#endif
