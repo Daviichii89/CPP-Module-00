fef183627cfe163963b9f699f6e29e29fc308113
David Fernandez <davichi89@gmail.com>
2025-03-17 22:17:58 +0100

    Create class Phonebook in the header file

---
+++ ex01/Phonebook.hpp
@@ -0,0 +1,18 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook {
+private:
+    Contact contacts[8];
+    int contactCount;
+
+public:
+    PhoneBook();
+    void addContact(std::string firstName, std::string lastName, std::string phone);
+    void displayContacts() const;
+    void displayContactDetails(int index) const;
+};
+
+#endif
